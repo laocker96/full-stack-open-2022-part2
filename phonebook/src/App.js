c32ae506d8cfbf6eac02b6cc7952b60ba691a303
Simone Bergamin <simone.bergamin2a@gmai.com>
2022-06-28 01:22:10 +0200

    Exercise 2.9

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('');
-  const [newPhone, setNewPhone] = useState('')
+  const [newPhone, setNewPhone] = useState('');
+  const [searchedName, setSearchedName] = useState('');
 
   const handleSubmit = (event) => {
 
@@ -19,7 +20,7 @@ const App = () => {
     } else {
       const person = {
         name: newName,
-        phone: newPhone
+        number: newPhone
       };
       setPersons(persons.concat(person));
       setNewName("");
@@ -35,9 +36,18 @@ const App = () => {
     setNewPhone(event.target.value);
   }
 
+  const handleSearchedNameChange = (event) => {
+    setSearchedName(event.target.value);
+  }
+
+  const filteredPersons = searchedName != "" ? persons.filter(person => person.name.toLowerCase().includes(searchedName.toLowerCase())) : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchedName} onChange={handleSearchedNameChange} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -50,11 +60,14 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {filteredPersons.map(person =>
         <p key={person.name}>
-          {person.name}&nbsp;{person.phone}
+          {person.name}&nbsp;{person.number}
         </p>
       )}
+      {filteredPersons.length == 0 &&
+        <p>No persons found</p>
+      }
     </div>
   )
 }
